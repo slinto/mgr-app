101f14a58f85e06d78b4c4f7595cf4193f3ca5e0
Tomáš Stankovič <hello@slinto.sk>
2017-04-07 23:11:20 +0200

    update config/api

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,10 @@
 export default {
-  tensorflow: 'https://slinto-mgr-tensorflow.herokuapp.com/api'
+  tensorflow: {
+    test: 'https://slinto-mgr-tensorflow.herokuapp.com/api',
+    production: 'https://slinto-mgr-tensorflow.herokuapp.com/api'
+  },
+  server: {
+    test: 'https://slinto-mgr-server-test.herokuapp.com/',
+    production: 'https://slinto-mgr-server-test.herokuapp.com/'
+  }
 };
